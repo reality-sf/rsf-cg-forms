dd719af0222153a54e6e85418eac0a689476924c
Daniel Chao <daniel.h.chao@gmail.com>
2019-02-09 11:11:41 -0800

    rm manifest, point client at staging env

--- src/clients/airtableProxyApi.js
+++ src/clients/airtableProxyApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const AIRTABLE_PROXY_URL = "https://rsf-airtable-proxy.herokuapp.com";
+const AIRTABLE_PROXY_URL = "https://staging-rsf-airtable-proxy.herokuapp.com";
 
 const http = axios.create({
   baseURL: AIRTABLE_PROXY_URL
