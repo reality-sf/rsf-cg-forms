5408ac3dc028cd36b707171cf52f7fb569611022
Daniel Chao <daniel.h.chao@gmail.com>
2019-07-20 09:41:16 -0700

    Updates

---
+++ precache-manifest.4e6455fcf13345b8cb0d22527895f296.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "83bc32a731c859ed536d",
+    "url": "./static/js/main.83bc32a7.chunk.js"
+  },
+  {
+    "revision": "189dccc295cb32675850",
+    "url": "./static/js/1.189dccc2.chunk.js"
+  },
+  {
+    "revision": "83bc32a731c859ed536d",
+    "url": "./static/css/main.76db6e65.chunk.css"
+  },
+  {
+    "revision": "f1aff92b8d281a2aadae16c59bdb3678",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rsf-cg-forms/precache-manifest.73e10bcb3e26c054104a040046a45383.js"
+  "./precache-manifest.4e6455fcf13345b8cb0d22527895f296.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/rsf-cg-forms/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
